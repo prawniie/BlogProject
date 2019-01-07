d4f1547c45af39a1544f74883bf235f39a74937d
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-07 09:39:40 +0100

    Initial commit

---
+++ Bloggy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bloggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
